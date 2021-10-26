f2ef494e4016119dd3f9b52884dc6f8d3194e5ed
Kep <KepCreations@gmail.com>
2021-10-25 21:58:08 -0400

    Clean up test file

--- test_user_model.py
+++ test_user_model.py
@@ -3,6 +3,7 @@
 # run these tests like:
 #
 #    python -m unittest test_user_model.py
+#    python -m unittest -v test_user_model.py   # For results of both success and fail tests
 
 
 import os
@@ -11,23 +12,9 @@ from sqlalchemy import exc
 
 from models import db, User, Message, Follows
 
-# BEFORE we import our app, let's set an environmental variable
-# to use a different database for tests (we need to do this
-# before we import our app, since that will have already
-# connected to the database
-
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
-
-# Now we can import app
-
 from app import app
-
-# Create our tables (we do this here, so we only create the tables
-# once for all tests --- in each test, we'll delete the data
-# and create fresh new clean test data
-
-db.create_all()
 
 DEFAULT_IMAGE_URL ="/static/images/default-pic.png"
 DEFAULT_HEADER_IMAGE_URL ="/static/images/warbler-hero.jpg"
@@ -37,6 +24,7 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
+
         db.drop_all()
         db.create_all()
 
@@ -66,10 +54,16 @@ class UserModelTestCase(TestCase):
         self.client = app.test_client()
 
     def tearDown(self):
+        """Rollback problems from failed tests"""
+
         db.session.rollback()
 
+    #=========================================================================================================
+    # Basic Model Tests    
+    #=========================================================================================================
+
     def test_user_model(self):
-        """Does basic model work?"""
+        """Test basic user model"""
 
         # User should have no messages & no followers
         self.assertEqual(len(self.u1.messages), 0)
